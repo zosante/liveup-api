9141ef62ed96ff4c2235dd90d7f85ed72055d30e
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2020-04-11 23:39:46 +0300

    Add symptoms and users seeders

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Database\Seeds\BaseSeeder;
+
+class UsersTableSeeder extends BaseSeeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\User::updateOrcreate([
+            'email' => 'test@example.com',
+        ], [
+            'name' => $this->faker->name,
+            'password' => bcrypt('KHJ97ysahisa8a09yo8diaugs8b'),
+        ]);
+
+        \App\User::updateOrCreate([
+            'email' => 'test2@example.com',
+        ], [
+            'name' => $this->faker->name,
+            'password' => bcrypt('KHJ97ysahisa8a09yo8diaugs8b'),
+        ]);
+    }
+}
