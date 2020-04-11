80d9bb5baf286b273d9de14bb99e416b37e64f27
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2020-04-11 23:49:43 +0300

    Add token to seeded user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\UserService;
 use Database\Seeds\BaseSeeder;
 
 class UsersTableSeeder extends BaseSeeder
@@ -16,13 +17,15 @@ class UsersTableSeeder extends BaseSeeder
         ], [
             'name' => $this->faker->name,
             'password' => bcrypt('KHJ97ysahisa8a09yo8diaugs8b'),
+            'api_token' => UserService::generateHashed(UserService::getRandomToken())
         ]);
 
         \App\User::updateOrCreate([
             'email' => 'test2@example.com',
         ], [
             'name' => $this->faker->name,
-            'password' => bcrypt('KHJ97ysahisa8a09yo8diaugs8b'),
+            'password' => bcrypt('KHJ97ysahsisa8a09yoa8diaugsaawe8b'),
+            'api_token' => UserService::generateHashed(UserService::getRandomToken()),
         ]);
     }
 }
