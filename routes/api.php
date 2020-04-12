3435ebf31e5afdec82e8d2e091b9d9d7935489d1
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2020-04-12 23:00:21 +0300

    Regroup routes

--- routes/api.php
+++ routes/api.php
@@ -14,42 +14,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return [
-        'Welcome to LiveUp API. Please check documentations or test you are connected with /api/tests endpoints.'
-    ];
-});
-
-Route::any('tests', function (Request $request) {
-    return [
-        'method' => $request->method(),
-        'data' => $request->all()
-    ];
-});
-
-Route::prefix('auth')->group(function () {
-    Route::post('register', 'AuthController@register');
-    Route::post('login', 'AuthController@login');
-});
-
-Route::middleware('auth:api')->name('api.')->group(function () {
-    Route::post('token/refresh', 'ApiTokenController@refresh');
-
-    Route::prefix('/user')->name('user.')->group(function () {
-        Route::get('', function (Request $request) {
-            return $request->user();
-        });
-
-        Route::post('groups', 'GroupsController@create');
-        Route::get('groups', 'GroupsController@getAll');
-        Route::post('groups/{group}', 'GroupUserController@addGroupUser');
-
-        Route::get('symptoms', 'UserSymptomsController@getAll');
-        Route::get('symptoms/{symptom}', 'UserSymptomsController@getOneFromList');
-        Route::post('symptoms/{symptom}/records', 'UserSymptomsController@addSymptomRecord');
-        Route::get('symptoms/{symptom}/records','UserSymptomsController@getSymptomRecords');
+Route::name('api.')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', 'AuthController@register');
+        Route::post('login', 'AuthController@login');
     });
 
-    Route::get('symptoms', 'SymptomsController@getAll');
-    Route::post('symptoms', 'SymptomsController@create');
+    Route::middleware('auth:api')->group(function () {
+        Route::post('token/refresh', 'ApiTokenController@refresh');
+
+        Route::prefix('user')->name('user.')->group(function () {
+            Route::get('', function (Request $request) {
+                return $request->user();
+            });
+
+            Route::post('groups', 'GroupsController@create');
+            Route::get('groups', 'GroupsController@getAll');
+            Route::post('groups/{group}', 'GroupUserController@addGroupUser');
+
+            Route::get('symptoms', 'UserSymptomsController@getAll');
+            Route::get('symptoms/{symptom}', 'UserSymptomsController@getOneFromList');
+            Route::post('symptoms/{symptom}/records', 'UserSymptomsController@addSymptomRecord');
+            Route::get('symptoms/{symptom}/records', 'UserSymptomsController@getSymptomRecords');
+        });
+
+        Route::get('symptoms', 'SymptomsController@getAll');
+        Route::post('symptoms', 'SymptomsController@create');
+    });
+
+    Route::get('/', function () {
+        return [
+            'Welcome to LiveUp API. Please check documentations or test you are connected with /api/tests endpoints.'
+        ];
+    });
+
+    Route::any('tests', function (Request $request) {
+        return [
+            'method' => $request->method(),
+            'data' => $request->all()
+        ];
+    });
 });
