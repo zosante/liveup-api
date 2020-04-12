54332a864ff80dcf722f2cbf2ceb0de7bb41fc85
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2020-04-12 13:19:30 +0300

    Auto create swagger.json after dockerizing

--- dockerize.sh
+++ dockerize.sh
@@ -17,3 +17,5 @@ fi
 docker-compose exec app php artisan db:seed --class=UsersTableSeeder
 
 docker-compose exec app php artisan db:seed --class=SymptomsTableSeeder
+
+php artisan laravel-swagger:generate  --filter="/api" > swagger.json
